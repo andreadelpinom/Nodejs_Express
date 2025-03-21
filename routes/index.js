b3ac58820e6efa5a5f0fa6ff41c76c37a5ab3e62
andreadelpinom <andreadelpino@uees.edu.ec>
2025-03-20 23:13:34 -0500

    carpeta fotos formulario y fotos formulario add en partials, intento de creacion de formulario

--- routes/index.js
+++ routes/index.js
@@ -19,5 +19,44 @@ router.get('/photos', async function(req, res) {
   }
 });
 
+router.get('/photos/add', function(req, res, next) {
+  res.render('fotos_formulario', { title: 'Express' });
+});
+
+router.post('/photos/save', async function(req, res, next) {
+  let { title, description, rate } = req.body;
+  
+  const URL = 'http://localhost:4444/rest/fotos/save';
+  
+  let data = {
+      titulo: title,
+      descripcion: description,
+      calificacion: parseFloat(rate),  // Asegurarse de que 'rate' sea un número
+      ruta: ''
+  };
+  
+  const config = {
+      proxy: {
+          host: 'localhost',
+          port: 4444
+      }
+  };
+  
+  try {
+      const response = await axios.post(URL, data, config);
+      
+      // Verifica si la respuesta tiene un estado 200
+      if (response.status === 200 && response.statusText === 'OK') {
+          res.redirect('/photos');
+      } else {
+          res.redirect('/');
+      }
+  } catch (error) {
+      console.error("Error al hacer la petición:", error.message);
+      res.redirect('/');  // En caso de error, redirige a la página de inicio
+  }
+});
+  
+
 // Exportamos el router correctamente
 module.exports = router;
